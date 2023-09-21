d3b8e09d1bb4f0dc05d0ae6c6fe1863ba00e8158
Ankush Chhabra <ankushchhabra2002@gmail.com>
2023-09-21 18:59:30 +0530

    first 3d project

---
+++ src/components/Map.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Annotation,
+  ZoomableGroup,
+} from "react-simple-maps";
+
+const Map = () => {
+  return (
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [0, 0, 0], // Reset rotation
+        center: [78.9629, 20.5937], // Center on India's coordinates
+        scale: 350, // Adjust the scale as needed
+      }}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <Geographies
+        geography="/features.json"
+        fill="#2C065D"
+        stroke="#FFFFFF"
+        strokeWidth={0.5}
+      >
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+      <Annotation
+        subject={[78.9629, 20.5937]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round",
+        }}
+      >
+        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"India"}
+        </text>
+      </Annotation>
+    </ComposableMap>
+  );
+};
+
+export default Map;
